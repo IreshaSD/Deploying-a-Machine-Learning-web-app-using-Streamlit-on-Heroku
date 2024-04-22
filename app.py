3f6d0d13ae9f8b7a02a354696f529a2e82ff10e4
Iresha Sandamali <152151059+IreshaSD@users.noreply.github.com>
2024-04-22 14:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -405,14 +405,7 @@ if (selected == 'Breast Cancer Predictions'):
 
         #breabreast_cancer_prediction =breast_cancer_model.predict([[mean_radius,mean_texture,mean_perimeter,mean_area,mean_smoothness,mean_compactness,mean_concavity,mean_concave_points,mean_symmetry,mean_fractal_dimension,radius_error,texture_error,perimeter_error,area_error,smoothness_error,compactness_error,concavity_error,concave_points_error,symmetry_error,fractal_dimension_error,worst_radius,worst_texture,worst_perimeter,worst_area,worst_smoothness,worst_compactness,worst_concavity,worst_concave_points,worst_symmetry,worst_fractal_dimension]])
         
-          if not all([float(mean_radius), float(mean_texture), float(mean_perimeter), float(mean_area),
-                              float(mean_smoothness), float(mean_compactness), float(mean_concavity), float(mean_concave_points),
-                              float(mean_symmetry), float(mean_fractal_dimension), float(radius_error), float(texture_error),
-                              float(perimeter_error), float(area_error), float(smoothness_error), float(compactness_error),
-                              float(concavity_error), float(concave_points_error), float(symmetry_error), float(fractal_dimension_error),
-                              float(worst_radius), float(worst_texture), float(worst_perimeter), float(worst_area),
-                              float(worst_smoothness), float(worst_compactness), float(worst_concavity), float(worst_concave_points),
-                              float(worst_symmetry), float(worst_fractal_dimension)]):
+          if not all([mean_radius,mean_texture,mean_perimeter,mean_area,mean_smoothness,mean_compactness,mean_concavity,mean_concave_points,mean_symmetry,mean_fractal_dimension,radius_error,texture_error,perimeter_error,area_error,smoothness_error,compactness_error,concavity_error,concave_points_error,symmetry_error,fractal_dimension_error,worst_radius,worst_texture,worst_perimeter,worst_area,worst_smoothness,worst_compactness,worst_concavity,worst_concave_points,worst_symmetry,worst_fractal_dimension]):
               st.warning('Please enter all values before proceeding.')
         
           else:
